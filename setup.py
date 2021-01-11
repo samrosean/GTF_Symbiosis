152da07d14677e3cba65b5af2173ba6b620d14e2
samissimas <samrosean@gmail.com>
2021-01-10 20:47:22 -0700

    changed name to GTF_Symbiosis

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gtfSymbiosis",
+    name="GTF_Symbiosis",
     version="0.0.1",
     author="Samuel Rosean",
     author_email="samrosean@gmail.com",
