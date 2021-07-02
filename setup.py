aaa58d6bed3ae1a21b7d4311d119ed13f397fd03
Samuel Noah Rosean <44511735+samrosean@users.noreply.github.com>
2021-07-01 18:25:57 -0600

    Update Setup with final name

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="GTF_Symbiosis",
+    name="gtfSymbiosis",
     version="0.0.1",
     author="Samuel Rosean",
     author_email="samrosean@gmail.com",
-    packages=['GTF_Symbiosis'],
+    packages=['gtfSymbiosis'],
     description="construction of transcriptome from GTF files, and comparison between GTf files",
     long_description=long_description,
     long_description_content_type="text/markdown",
