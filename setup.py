89a1374a82c906b1243d9425253d4e14e4ed8fb2
Samuel Noah Rosean <44511735+samrosean@users.noreply.github.com>
2022-04-17 15:23:42 -0600

    setup fixes

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name = 'gtfSymbiosis', version = '0.0.2', author = 'Samuel Rosean', author_email = 'samrosean@gmail.com', description = 'construction of transcriptome from GTF files, and comparison between GTF files', url = 'https://github.com/samrosean/GTF_Symbiosis', packages=find_packages(),)
+setup(name = 'gtfSymbiosis', version = '0.0.2', author = 'Samuel Rosean', author_email = 'samrosean@gmail.com', description = 'construction of transcriptome from GTF files, and comparison between GTF files', url = 'https://github.com/samrosean/GTF_Symbiosis', packages=find_packages(','),install_requires=['<pandas>', '<numpy>', '<random>', '<matplotlib>', '<matplotlib_venn>', '<prettytable>', '<math>', '<prettytable>', '<networkx>', '<venn>', '<upsetplot>', '<seaborn>', '<pyranges>', '<time>', '<collections>', '<itertools>'] )
 
 
